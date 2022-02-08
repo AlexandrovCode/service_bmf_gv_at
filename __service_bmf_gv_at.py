416acb5224111e05be4a0e435b968466f6db29a3
AlexandrovCode <mathymath24@gmail.com>
2022-02-08 16:38:08 +0300

    Initial commit

---
+++ __service_bmf_gv_at.py
@@ -0,0 +1,14 @@
+import time
+import json
+from service_bmf_gv_at import *
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    a = Handler()
+
+    final_data = a.Execute('GAMEX Personalbereitstellung GmbH', '', '', '')
+    print(json.dumps(final_data, indent=4))
+
+    elapsed_time = time.time() - start_time
+    print('\nTask completed - Elapsed time: ' + str(round(elapsed_time, 2)) + ' seconds')
